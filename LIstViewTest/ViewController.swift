6ad3e405dbd31f6a5eefaa06e76754f0a3c68482
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-03-04 02:40:03 +0900

    [main] XMLParser를 이용한 컬렉션뷰 구현

--- LIstViewTest/ViewController.swift
+++ LIstViewTest/ViewController.swift
@@ -17,16 +17,21 @@ struct FiltersData: Codable{
 
 
 
-struct Test: Codable{
+struct Test{
     let name : String
     let category: String
     let effect : String
     let parameters : [String: Int]
 }
 
-struct TestData2: Codable{
-    let tests : [Test]
+struct XMLFilter {
+    var name: String = ""
+    var category: String = ""
+    var effect: String = ""
+    var parameters: [String: String] = [:]
 }
+
+
 
 
 
@@ -34,9 +39,10 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     var filterList: [Filter] = [] // 필터 데이터를 저장할 배열 (Codable)
     var testList: [Test] = [] // 필터 데이터를 저장할 배열 (JsonSerialization)
+    var xmlFiltersList: [XMLFilter] = []
     var collectionView: UICollectionView!
     var collectionView2: UICollectionView!
-    
+    var collectionView3: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,9 +73,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         setupCollectionView2()
         setupCollectionView()
+        setupCollectionView3()
         
         loadFiltersData()
         loadFiltersDataSerialization()
+        loadXMLFiltersData()
         
         view.addSubview(text1)
         view.addSubview(text2)
@@ -127,6 +135,82 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             print("Error parsing JSON: \(error)")
         }
     }
+    
+    func loadXMLFiltersData() {
+        if let url = Bundle.main.url(forResource: "filters", withExtension: "xml"),
+           let data = try? Data(contentsOf: url) {
+            print("XML 데이터 로드 성공") // 성공적으로 로드되었는지 확인
+            let parser = XMLFiltersParser()
+            let xmlFilters = parser.parse(data: data)
+            self.xmlFiltersList = xmlFilters
+            collectionView3.reloadData()
+        } else {
+            print("XML 데이터 로드 실패") // 파일 로드 실패 시
+        }
+    }
+
+    
+    class XMLFiltersParser: NSObject, XMLParserDelegate {
+        var filters: [XMLFilter] = []
+        var currentElement = ""
+        var currentFilter: XMLFilter?
+        var currentValue = ""
+
+        // 추가된 parse(data:) 메서드
+        func parse(data: Data) -> [XMLFilter] {
+            let parser = XMLParser(data: data)
+            parser.delegate = self
+            parser.parse()
+            return filters
+        }
+
+        // 태그의 시작
+        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+            
+            print("시작 태그: \(elementName)")
+            
+            currentElement = elementName
+            if elementName == "filter" {
+                currentFilter = XMLFilter()
+            }
+        }
+
+        // 태그 사이의 문자열
+        func parser(_ parser: XMLParser, foundCharacters string: String) {
+            
+            print("찾은 문자열: \(string)")
+            
+            currentValue += string
+        }
+
+        // 태그의 끝
+        func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            
+            print("종료 태그: \(elementName)")
+            
+            switch elementName {
+            case "filter":
+                if let filter = currentFilter {
+                    filters.append(filter)
+                }
+                currentFilter = nil
+            case "name":
+                currentFilter?.name = currentValue.trimmingCharacters(in: .whitespacesAndNewlines)
+            case "category":
+                currentFilter?.category = currentValue.trimmingCharacters(in: .whitespacesAndNewlines)
+            case "effect":
+                currentFilter?.effect = currentValue.trimmingCharacters(in: .whitespacesAndNewlines)
+            default:
+                if currentElement == "parameters" && elementName != "parameters" {
+                    currentFilter?.parameters[elementName] = currentValue.trimmingCharacters(in: .whitespacesAndNewlines)
+                }
+            }
+            currentValue = ""
+        }
+    }
+
+    
+    
     
     func loadFiltersDataSerialization() {
         guard let url = Bundle.main.url(forResource: "filters", withExtension: "json") else {
@@ -193,13 +277,35 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         view.addSubview(collectionView2)
         
         NSLayoutConstraint.activate([
-            collectionView2.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            collectionView2.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
             collectionView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             collectionView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             collectionView2.heightAnchor.constraint(equalToConstant: 150)
         ])
         
         collectionView2.backgroundColor = .gray
+    }
+    
+    func setupCollectionView3() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 120, height: 140)
+        
+        collectionView3 = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView3.translatesAutoresizingMaskIntoConstraints = false
+        collectionView3.register(FilterButtonCell.self, forCellWithReuseIdentifier: "FilterButtonCell")
+        collectionView3.dataSource = self
+        collectionView3.delegate = self
+        view.addSubview(collectionView3)
+        
+        NSLayoutConstraint.activate([
+            collectionView3.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 100),
+            collectionView3.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            collectionView3.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            collectionView3.heightAnchor.constraint(equalToConstant: 150)
+        ])
+        
+        collectionView3.backgroundColor = .gray
     }
     
     
@@ -213,6 +319,12 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         else if collectionView == self.collectionView2 {
             return testList.count
         }
+        
+        else if collectionView == self.collectionView3 {
+            print(xmlFiltersList.count)
+            return xmlFiltersList.count
+        }
+        
         return 0
     }
 
@@ -237,7 +349,22 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             cell.configure(with: UIImage(named: "input2.jpg")!, name: "\(test.name)", category: "\(test.category)")
             return cell
         }
+        
+        
+        else if collectionView == self.collectionView3 {
+            // collectionView2에 대한 셀 구성
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterButtonCell", for: indexPath) as? FilterButtonCell else {
+                fatalError("Unable to dequeue FilterButtonCell")
+            }
+            let xmlFilter = xmlFiltersList[indexPath.row]
+            cell.configure(with: UIImage(named: "input2.jpg")!, name: "\(xmlFilter.name)", category: "\(xmlFilter.category)")
+            return cell
+        }
+        
+        
         fatalError("Unexpected collectionView")
+        
+        
     }
     
     
@@ -256,6 +383,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             let selectedFilter : Test = testList[indexPath.row]
             
             let message = "JsonSerialization Button \(selectedFilter.name) clicked"
+            showToast(message: message, seconds: 0.5)
+        }
+        
+        else if collectionView == self.collectionView3{
+            let selectedFilter : Test = testList[indexPath.row]
+            
+            let message = "XMLParser Button \(selectedFilter.name) clicked"
             showToast(message: message, seconds: 0.5)
         }
         
